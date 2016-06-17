4e7362d16648b0cf9cd79ac5253f001cc7b999aa
Felipe Vieira <felipe29vieira@gmail.com>
2016-06-17 14:21:33 -0300

    storing game's scores on database

--- crawler.py
+++ crawler.py
@@ -2,12 +2,8 @@ import urllib
 import sqlite3
 from html_parser import SuperPlacarParser
 
-teams_db = sqlite3.connect("teams.db", check_same_thread=False)
-teams_cur = teams_db.cursor()
-teams_cur.execute('CREATE TABLE IF NOT EXISTS teams (user_id INTEGER PRIMARY KEY, name TEXT, last_score TEXT, last_score_board TEXT)')
-
 def crawl():
-	url = "http://www.superplacar.com.br/partidas-anteriores/2016-06-15"
+	url = "http://www.superplacar.com.br"
 	page = urllib.urlopen(url)
 	content = page.read()
 
--- html_parser.py
+++ html_parser.py
@@ -1,9 +1,21 @@
 from bs4 import BeautifulSoup
+import sqlite3
+import datetime
 
 # create a subclass and override the handler methods
 class SuperPlacarParser():
 	def __init__(self, content):
 		self.soup = BeautifulSoup(content, 'html.parser')
+		self.create_schema()
+
+	def create_schema(self):
+		self.games_db = sqlite3.connect("games.db", check_same_thread=False)
+		self.games_cur = self.games_db.cursor()
+		self.games_cur.execute('CREATE TABLE IF NOT EXISTS game (match_day DATE, home_team_name TEXT, away_team_name TEXT, score TEXT, score_board_home TEXT, score_board_away TEXT, score_board_times_home TEXT, score_board_times_away TEXT, notify BOOLEAN, PRIMARY KEY (match_day, home_team_name, away_team_name))')
+		self.games_db.commit()
+
+		#for row in self.games_cur.execute('SELECT * FROM game'):
+        		#print row
 
 	def parse_scores(self):
 		counter = 0
@@ -16,10 +28,10 @@ class SuperPlacarParser():
 				scorer_parser = BeautifulSoup("<html> " + str(scorers_tags) + " </html>", 'html.parser')
 
 				for scorer in scorer_parser.find_all("span", { "class" : "goal-player"} ):
-					scorers_home.append(scorer.contents[0])
+					scorers_home.append(scorer.contents[0].strip())
 
 				for goal_time in scorer_parser.find_all("span", { "class" : "goal-time"} ):
-					scorers_home_time.append(goal_time.contents[0])
+					scorers_home_time.append(goal_time.contents[0].strip())
 
 				scorers_away = []
 				scorers_away_time = []
@@ -28,26 +40,43 @@ class SuperPlacarParser():
 				scorer_parser = BeautifulSoup("<html> " + str(scorers_tags) + " </html>", 'html.parser')
 
 				for scorer in scorer_parser.find_all("span", { "class" : "goal-player"} ):
-					scorers_away.append(scorer.contents[0])
+					scorers_away.append(scorer.contents[0].strip())
 
 				for goal_time in scorer_parser.find_all("span", { "class" : "goal-time"} ):
-					scorers_away_time.append(goal_time.contents[0])
+					scorers_away_time.append(goal_time.contents[0].strip())
 
 
 				team_home = self.soup.find_all("div", { "class" : "team-name"} )[counter].children.next().children.next().contents[0].strip()
 				team_away = self.soup.find_all("div", { "class" : "team-name"} )[counter+1].children.next().children.next().contents[0].strip()
-				score_home = int(self.soup.find_all("div", { "class" : "team-score"} )[counter].contents[0].encode('utf-8'))
-				score_away = int(self.soup.find_all("div", { "class" : "team-score"} )[counter+1].contents[0].encode('utf-8'))
-				print "Score: %s %ix%i %s" % (team_home, score_home, score_away, team_away)
-				print "Scorers from home team: " + str(scorers_home)
-				print "Goal times: " + str(scorers_home_time)
-				print "Scorers from away team: " + str(scorers_away)
-				print "Goal times: " + str(scorers_away_time)
+				 
+				score_home = int(self.soup.find_all("div", { "class" : "team-score"} )[counter].contents[0].encode('utf-8')) if len(self.soup.find_all("div", { "class" : "team-score"} )[counter].contents) > 0 else None
+				score_away = int(self.soup.find_all("div", { "class" : "team-score"} )[counter+1].contents[0].encode('utf-8')) if len(self.soup.find_all("div", { "class" : "team-score"} )[counter+1].contents) > 0 else None
 
-				#TODO store values on database for further querying
-				print 
+				if score_home != None and score_away != None:
+					self.insert_or_update_game_in_database(team_home, team_away, score_home, score_away, scorers_home, scorers_away, scorers_home_time, scorers_away_time)
 
-			except:
+			except Exception,e:
+				print 'er:', e.message
 				continue
 			finally:
 				counter+=2
+	
+	def insert_or_update_game_in_database(self, team_home, team_away, score_home, score_away, scorers_home, scorers_away, scorers_home_time, scorers_away_time):
+		dt = datetime.datetime.now()		
+		today = datetime.date(dt.year, dt.month, dt.day)
+		
+		# If the game still doesn't exist on database, add it to the records
+		try:
+
+			self.games_cur.execute("INSERT INTO game VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (today, team_home, team_away, str(score_home) + "x" + str(score_away), ",".join(scorers_home), ",".join(scorers_away), ",".join(scorers_home_time), ",".join(scorers_away_time), True))
+			self.games_db.commit()
+
+		# Otherwise, check is the scoreboard changed and update game highlights
+		except:
+			self.games_cur.execute('SELECT * FROM game WHERE match_day=? AND home_team_name=? AND away_team_name=?', (today, team_home, team_away))
+			current_game = self.games_cur.fetchone()
+			
+			if current_game[3] != "%ix%i" % (score_home, score_away):
+				self.games_cur.execute("UPDATE game SET score=?, score_board_home=?, score_board_away=?, score_board_times_home=?, score_board_times_away=?, notify=? WHERE match_day=? AND home_team_name=? AND away_team_name=?", ("%ix%i" % (score_home, score_away), ",".join(scorers_home), ",".join(scorers_away), ",".join(scorers_home_time), ",".join(scorers_away_time), True, today, team_home, team_away))
+				self.games_db.commit()
+
